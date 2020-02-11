ce6262f06b644df9b3d491964a279803bc87d16a
usmanjaved123 <usmanjaved4076@gmail.com>
2020-02-11 22:40:02 +0500

    2/11/2020
    added traps

--- Assets/SpikePlayerCollisionScript.cs
+++ Assets/SpikePlayerCollisionScript.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class SpikePlayerCollisionScript : MonoBehaviour {
 
+    [SerializeField]
+    private GameObject blood;
     void OnTriggerEnter2D(Collider2D col)
     {
         //Check if enemy is attacking
         if (col.CompareTag("Player"))
         {
+            //Show Blood
+            Instantiate(blood, col.gameObject.transform.position, Quaternion.identity);
             HealthBarScript.health -= 100f;
         }
     }
